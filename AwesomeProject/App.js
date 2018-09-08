a63cd568d05197e344649ff2c74333057cc8ea2b
Juan Reyes <reyes1212@gmail.com>
2018-09-07 20:06:53 -0700

    Updated styles, added greetings component,  and image
    different way to style, different component types/elements, props and importing examples.

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-export default class App extends React.Component {
+import Greeting from './components/greeting.js';
+
+export default class App extends Component {
   render() {
+    const bananaPicUrl = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          title='Test Button'
+          color='#3279ad'
+          accessibilityLabel='Learn more about this blue button'
+        />
+        <Text style={styles.h2}>Hello, World!</Text>
+        <Text style={styles.text}>To start things off, here is a picture of some bananas:</Text>
+        <Image source={bananaPicUrl} style={styles.image} />
+        <Text style={styles.text}>... and here are some people who love them:</Text>
+        <Greeting name='Plam' />
+        <Greeting name='Plob' />
+        <Greeting name='Pleb' />
+        <Greeting name='Plake' />
       </View>
     );
   }
@@ -16,8 +31,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#febebc',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#19405e',
+    fontSize: 20,
+  },
+  h2: {
+    fontSize: 30
+  },
+  image: {
+    width: 200,
+    height: 120,
+  }
 });
